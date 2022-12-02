6491a93d61358cf577a49bd3bdcdbce5849217b3
oluwasube <oluwasube@outlook.com>
2022-12-02 12:47:56 +0100

    task completed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at leaset 1 child in a binary tree
- * @tree: A pointer to the root node of the tree to count the nodes
- *
- * Return: The count or 0 if tree is NULL
+ * binary_tree_nodes - function that measures the nodes of a binary tree
+ * @tree: pointer to the root node of the tree to to measure the nodes
+ * Return: nodes of the tree else 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t nodes;
 
-	if (!tree || (!tree->left && !tree->right))
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
 		return (0);
 
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
-
-	return (count + 1);
+	nodes = binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1;
+	return (nodes);
 }
